256460cfb54ca0ba0fe793d43eda98aa2c40dad8
mbrown <tugtuj@gmail.com>
2021-02-11 22:09:04 -0500

    Images up!

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const expressEjsLayouts = require('express-ejs-layouts')
-const db =require('../models')
-
-const ejs = require('ejs')
+const db =require('./models')
 const axios = require('axios')
 const morgan = require('morgan')
+
 
 
 const app = express()
@@ -13,20 +13,28 @@ app.use(express.static('public'))
 
 //Templating engine
 app.use(express.urlencoded({ extended: false}))
+//not sure what this even does
 
 app.set('view engine', 'ejs')
 
 //route
 app.get('/', (req, res) => {
-    const artUrl = 'https://www.rijksmuseum.nl/api/nl/collection/SK-C-5/tiles?key=JMAXiERl'
-    //'/api/en/collection/4/tiles'
-    
+    const API_key = process.env.API_key
+    //const artUrl = `https://www.rijksmuseum.nl/api/en/collection/SK-C-5?key=${API_key}&f.dating.period=21`
+    const artUrl = `https://www.rijksmuseum.nl/api/en/collection/?key=${API_key}`
+    //'/api/en/collection/4/tiles' having trouble accessing photos
     axios.get(artUrl).then(function(apiResponse) {
-        const art = apiResponse
+        const artData = apiResponse.data.artObjects
+        const artImage = apiResponse.data.artObject[0].webImage.url
         console.log("**********************************************")
-        console.log(art.levels[0].tiles[0].url)
-        res.render('index')
-        d
+        console.log(artData)
+        console.log(artImage)
+        res.render('index', { image })
+       //db.painting.create ??
+    })
+    .catch(error => {
+        console.log(`Uh-oh. There's an error here.`)
+        console.log(error)
     })
 })
 
